3c84396bfd971e4dcf8286afd389f1157db8f504
jessemcg <jessemcg@gmail.com>
2024-01-09 07:38:10 -0800

    modify start server file

--- start_server.sh
+++ start_server.sh
@@ -4,5 +4,4 @@
 cd $HOME/whisper.cpp
 
 # Start the optimized ggml small Whisper model in server mode and keep it running in the background
-./server -ng -l en -t 12 -m models/ggml-small.en.bin --convert &
-#./server -ng -l en -m models/ggml-small.en.bin --convert &
+./server -ng -l en -m models/ggml-small.en.bin --convert &
